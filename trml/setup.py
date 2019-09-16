f50c84533cff0f754b9fc8f1dd2f521e09f43dc4
Dmitry Ryumin <dl_03.03.1991@mail.ru>
2019-09-16 22:56:40 +0300

    Summary

--- trml/setup.py
+++ trml/setup.py
@@ -1,30 +1,42 @@
 from setuptools import setup, find_packages
 
-project_name = 'trml'
+import trml
 
 REQUIRED_PACKAGES = [
 
 ]
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
-
-setup(
-    name = project_name,
-    packages = find_packages(),
-    license = 'MIT',
-    version = "0.0.1rc2",
-    author = "Dmitry Ryumin",
-    author_email = "dl_03.03.1991@mail.ru",
-    description = "Shell",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    install_requires=REQUIRED_PACKAGES,
-    keywords = ['shell'],
-    classifiers = [
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires = '>=3.7',
-)
+    setup(
+        name = trml.__name__,
+        packages = find_packages(),
+        license = trml.__license__,
+        version = trml.__version__,
+        author = trml.__author__,
+        author_email = trml.__email__,
+        url = trml.__uri__,
+        description = trml.__summary__,
+        long_description = long_description,
+        long_description_content_type = 'text/markdown',
+        install_requires=REQUIRED_PACKAGES,
+        keywords = ['shell'],
+        platforms = ['Windows', 'MacOS X'],
+        classifiers = [
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: Russian',
+            'Natural Language :: English',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Microsoft :: Windows',
+            'Programming Language :: Python :: 3.7',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'Intended Audience :: Science/Research',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Software Development',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+        python_requires = '>=3.7',
+    )
---
+++ trml/trml/__init__.py
@@ -0,0 +1,15 @@
+__all__ = (
+    '__title__', '__summary__', '__uri__', '__version__', '__author__', '__email__', '__license__', '__copyright__',
+)
+
+__title__ = 'trml'
+__summary__ = 'Работа с Shell'
+__uri__ = 'https://github.com/DmitryRyumin/pkgs/tree/master/trml'
+
+__version__ = '2019.9.16rc0'
+
+__author__ = 'Dmitry Ryumin'
+__email__ = 'dl_03.03.1991@mail.ru'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2019 Dmitry Ryumin'
