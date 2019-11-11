73f848b7a6f6d7a417e5078b22e10f10a02beac3
Dmitry Ryumin <dl_03.03.1991@mail.ru>
2019-11-11 13:04:28 +0300

    Summary

--- trml/setup.py
+++ trml/setup.py
@@ -1,10 +1,41 @@
+import sys
+
 from setuptools import setup, find_packages
+
+MIN_PYTHON_VERSION = (3, 7)
+
+if sys.version_info[:2] < MIN_PYTHON_VERSION:
+    raise RuntimeError("Требуется версия Python >= {}.{}".format(MIN_PYTHON_VERSION[0], MIN_PYTHON_VERSION[1]))
 
 import trml
 
 REQUIRED_PACKAGES = [
 
 ]
+
+CLASSIFIERS = """\
+Development Status :: 5 - Production/Stable
+Natural Language :: Russian
+Natural Language :: English
+Intended Audience :: Developers
+Intended Audience :: Education
+Intended Audience :: Science/Research
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3 :: Only
+Programming Language :: Python :: Implementation :: CPython
+Topic :: Scientific/Engineering
+Topic :: Scientific/Engineering :: Mathematics
+Topic :: Software Development
+Topic :: Software Development :: Libraries
+Topic :: Software Development :: Libraries :: Python Modules
+Operating System :: MacOS :: MacOS X
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX :: Linux
+"""
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
@@ -23,27 +54,8 @@ with open('README.md', 'r') as fh:
         long_description = long_description,
         long_description_content_type = 'text/markdown',
         install_requires=REQUIRED_PACKAGES,
-        keywords = ['shell'],
-        classifiers = [
-            'License :: OSI Approved :: MIT License',
-            'Natural Language :: Russian',
-            'Natural Language :: English',
-            'Operating System :: MacOS :: MacOS X',
-            'Operating System :: Microsoft :: Windows',
-            "Operating System :: POSIX :: Linux",
-            "Programming Language :: Python",
-            'Programming Language :: Python :: 3.7',
-            "Programming Language :: Python :: Implementation :: CPython",
-            "Development Status :: 5 - Production/Stable",
-            'Intended Audience :: Developers',
-            'Intended Audience :: Education',
-            'Intended Audience :: Science/Research',
-            'Topic :: Scientific/Engineering',
-            'Topic :: Scientific/Engineering :: Mathematics',
-            'Topic :: Software Development',
-            'Topic :: Software Development :: Libraries',
-            'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
+        keywords = ['trml'],
+        classifiers = [_f for _f in CLASSIFIERS.split('\n') if _f],
         python_requires = '>=3.7',
         entry_points = {
             'console_scripts': [
--- trml/trml/__init__.py
+++ trml/trml/__init__.py
@@ -7,7 +7,7 @@ __title__ = 'trml'
 __summary__ = 'Работа с Shell'
 __uri__ = 'https://github.com/DmitryRyumin/pkgs/tree/master/trml'
 
-__version__ = '19.10.4.0'
+__version__ = '19.11.11.0'
 
 __author__ = 'Dmitry Ryumin'
 __email__ = 'dl_03.03.1991@mail.ru'
