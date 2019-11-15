b624168e3e3976a0dccfb19ad5f5327f8776e953
Dmitry Ryumin <dl_03.03.1991@mail.ru>
2019-11-15 18:21:03 +0300

    Summary

--- objdet/objdet/samples/detection.py
+++ objdet/objdet/samples/detection.py
@@ -277,12 +277,12 @@ class Run(Messages):
         Возвращает: True если аргументы переданы верно, в обратном случае False
         """
 
+        curr_path_to_model = self._args['path_to_model']  # Текущий путь к модели
+        curr_path_to_labels = self._args['path_to_labels']  # Текущий путь к конфигурационному файлу модели
+
         # Выполнение функции из суперкласса с отрицательным результатом
         if super()._update_config_json(set_window_name) is False:
             return False
-
-        curr_path_to_model = self._args['path_to_model']  # Текущий путь к модели
-        curr_path_to_labels = self._args['path_to_labels']  # Текущий путь к конфигурационному файлу модели
 
         if self._automatic_update['invalid_config_file'] is False:
             # 1. Путь к модели был изменен
